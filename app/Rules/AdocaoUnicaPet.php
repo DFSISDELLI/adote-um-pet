46941ba431092fed6fbb02aede181e20d1de1d04
DSISDELLI <sisdellidafne@gmail.com>
2022-07-26 11:23:58 -0300

    fix - name class adocaoUnicaPet

--- app/Rules/AdocaoUnicaPet.php
+++ app/Rules/AdocaoUnicaPet.php
@@ -5,7 +5,7 @@ namespace App\Rules;
 use App\Models\Adocao;
 use Illuminate\Contracts\Validation\Rule;
 
-class AdocaoUnicaPet implements Rule
+class adocaoUnicaPet implements Rule
 {
     /**
      * Create a new rule instance.
